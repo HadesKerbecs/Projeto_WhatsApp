2ddd3b2af57efe66197409fe73d5bcfd803ecc72
Eduardo <eduardotm22@gmail.com>
2025-06-25 19:32:03 -0300

    Configurando db.js do Backend

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,19 +1,15 @@
-// utils/db.js - Conexão com MongoDB usando Mongoose
-
 const mongoose = require('mongoose');
-
-const MONGO_URI = 'mongodb://localhost:27017/seu_banco_de_dados'; // Troque para sua URI MongoDB, ex: Mongo Atlas
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB conectado com sucesso!');
+    console.log('MongoDB conectado!');
   } catch (error) {
-    console.error('Erro ao conectar no MongoDB:', error);
-    process.exit(1); // Sai do processo com erro
+    console.error('Erro ao conectar no MongoDB', error);
+    process.exit(1);
   }
 };
 
