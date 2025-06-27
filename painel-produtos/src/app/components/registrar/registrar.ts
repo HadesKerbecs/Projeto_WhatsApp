c58f90e0a28ddaf99488773e48169cb706f125ef
Eduardo <eduardotm22@gmail.com>
2025-06-27 19:22:42 -0300

    Corrigindo Erros

--- painel-produtos/src/app/components/registrar/registrar.ts
+++ painel-produtos/src/app/components/registrar/registrar.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
-  imports: [],
+  imports: [CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,],
   templateUrl: './registrar.html',
   styleUrl: './registrar.scss'
 })
@@ -12,10 +21,10 @@ export class Registrar {
   username = '';
   password = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) { }
 
-  registrar():void {
-    const user = { username: this.username, password: this.password};
+  registrar(): void {
+    const user = { username: this.username, password: this.password };
     this.authService.registrar(user).subscribe({
       next: () => this.router.navigate(['/login']),
       error: () => alert('Erro ao registrar usuário!')
