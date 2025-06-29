42780144ae5c9999567046e913424ed613a7eac5
Eduardo <eduardotm22@gmail.com>
2025-06-29 19:40:35 -0300

    Corrige importação do authMiddleware nas rotas de mensagem

--- backend/routes/mensagemRoutes.js
+++ backend/routes/mensagemRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mensagemController = require('../controllers/mensagemController');
+const authMiddleware = require('../middleware/auth');
 router.use(authMiddleware);
 
 router.get('/', mensagemController.listarMensagens);
